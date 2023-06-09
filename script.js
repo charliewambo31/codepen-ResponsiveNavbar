ef1bb31d5aadb6ed3f132324fc304cd95b67be8d
charliewambo31 <111303001+charliewambo31@users.noreply.github.com>
2023-06-09 11:21:26 -0400

    commit

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ hamburger.addEventListener('click', ()=>{
 
 
 
-$("#search-icon").click(function() {
+$('#search-icon').click(function() {
     $(".nav").toggleClass("search");
     $(".nav").toggleClass("no-search");
     $(".search-input").toggleClass("search-active");
@@ -25,3 +25,62 @@ $("#search-icon").click(function() {
      $(".nav").toggleClass("mobile-nav");
      $(this).toggleClass("is-active");
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $(document).ready(function() {
+    $("#myCarousel").on("slide.bs.carousel", function(e) {
+      var $e = $(e.relatedTarget);
+      var idx = $e.index();
+      var itemsPerSlide = 3;
+      var totalItems = $(".carousel-item").length;
+  
+      if (idx >= totalItems - (itemsPerSlide - 1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i = 0; i < it; i++) {
+          // append slides to end
+          if (e.direction == "left") {
+            $(".carousel-item")
+              .eq(i)
+              .appendTo(".carousel-inner");
+          } else {
+            $(".carousel-item")
+              .eq(0)
+              .appendTo($(this).find(".carousel-inner"));
+          }
+        }
+      }
+    });
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+
